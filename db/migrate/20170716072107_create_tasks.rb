2df632c1cbabdb31448097cc3d309c7cba53b853
Tatsuki Ishibashi <isshii3084352@gmail.com>
2017-07-16 07:29:35 +0000

    generate Tasks controller

--- db/migrate/20170716072107_create_tasks.rb
+++ db/migrate/20170716072107_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.boolean :done
+      t.boolean :done, default: false
       t.references :project, foreign_key: true
 
       t.timestamps
