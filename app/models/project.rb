c010ae1e41d9235dd9184d7092a0e22833948cef
Tatsuki Ishibashi <isshii3084352@gmail.com>
2017-07-16 07:22:16 +0000

    generate Task moder

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+  validates :title,
+  presence: { message: "エラー：プロジェクト名が空白です。"},
+  length: { minimum: 3, maximum: 20, message: "エラー：プロジェクト名は最小3文字、最大20文字です。" }
+  # project.title は空白NGと設定した。
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :projects
+
+#projectsのindexアクションをrootにする。
+  root 'projects#index'
 end
+
+# resources :projects 記述後、端末で $ rails routes の実行結果：
+#
+#       Prefix Verb   URI Pattern                  Controller#Action
+#     projects GET    /projects(.:format)          projects#index
+#             POST   /projects(.:format)          projects#create
+# new_project GET    /projects/new(.:format)      projects#new
+# edit_project GET    /projects/:id/edit(.:format) projects#edit
+#     project GET    /projects/:id(.:format)      projects#show
+#             PATCH  /projects/:id(.:format)      projects#update
+#             PUT    /projects/:id(.:format)      projects#update
+#             DELETE /projects/:id(.:format)      projects#destroy
+
+# Prefix            : ***
+# Verb              : ***
+# URI Pattern       : ***
+# Controller#Action : ***
